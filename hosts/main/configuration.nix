7aac18cb67eb74dd4b5cf54b65ab5f767a51b662
teodord25 <djuric.teodor25@gmail.com>
2024-09-02 19:31:10 +0200

    gen 19

--- hosts/main/configuration.nix
+++ hosts/main/configuration.nix
@@ -85,6 +85,24 @@
     swww
 
     rofi-wayland
+
+    gcc
+
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-emoji
+  ];
+
+  fonts.fonts = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk
+    # noto-fonts-emoji
+    # liberation_ttf
+    # fira-code
+    # fira-code-symbols
+    # mplus-outline-fonts.githubRelease
+    # dina-font
+    # proggyfonts
   ];
 
   xdg.portal.enable = true;
