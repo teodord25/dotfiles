46471026517173d611428b5d960a8f4d90c84214
teodord25 <djuric.teodor25@gmail.com>
2024-09-02 18:27:34 +0200

    gen 15

--- hosts/main/configuration.nix
+++ hosts/main/configuration.nix
@@ -66,6 +66,9 @@
     };
   };
 
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [];
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
