10adb62a3dd3e94691e4249cd33bdb5add811f7f
teodord25 <djuric.teodor25@gmail.com>
2024-09-02 14:08:50 +0200

    basic initial stable config with working hyprland and bluetooth

---
+++ NOTES.md
@@ -0,0 +1,3 @@
+# Notes to myself
+- clean up .ssh unused keys and config
+- figure out how to use home manager to config hyprland, neovim, tmux, nushell
---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations = {
+      main = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs;};
+        modules = [
+          ./hosts/main/configuration.nix
+          inputs.home-manager.nixosModules.default
+        ];
+      };
+      work = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs;};
+        modules = [
+          ./hosts/work/configuration.nix
+          inputs.home-manager.nixosModules.default
+        ];
+      };
+    };
+  };
+}
