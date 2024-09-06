7fb7be2132609d437f7413f17dc5172d65928134
teodord25 <djuric.teodor25@gmail.com>
2024-09-06 15:35:57 +0200

    gen 40

--- hosts/main/home.nix
+++ hosts/main/home.nix
@@ -8,6 +8,8 @@ in {
   programs.kitty.enable = true;
   home.sessionVariables.NIXOS_OZONE_WL = "1";
 
+  home-manager.backFileExtension = "backup";
+
   wayland.windowManager.hyprland = {
     enable = true;
 
