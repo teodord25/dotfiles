3b9deac8c6f6d9a252c5e7154bae3ea7b6eaa47b
teodord25 <djuric.teodor25@gmail.com>
2024-09-06 19:47:09 +0200

    gen 76

--- hosts/main/home.nix
+++ hosts/main/home.nix
@@ -18,7 +18,7 @@
   home.stateVersion = "24.05"; # Please read the comment before changing.
 
   # TODO: figure out why enabling this makes home manager fail to start (removing programs.hyperland.enable = true; in configuration.nix does not work)
-  wayland.windowManager.hyprland.enable = true;
+  # wayland.windowManager.hyprland.enable = true;
   wayland.windowManager.hyprland.settings = {
     "$mod" = "SUPER";
 
