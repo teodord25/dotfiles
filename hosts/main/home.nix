283948d856fefa932aab8e611c680b71e6e1c941
teodord25 <djuric.teodor25@gmail.com>
2024-09-07 11:35:00 +0200

    gen 81

--- hosts/main/home.nix
+++ hosts/main/home.nix
@@ -1,43 +1,128 @@
-{
-  config,
-  pkgs,
-  ...
-}: {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
+{...}: {
   home.username = "bane";
   home.homeDirectory = "/home/bane";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "24.05"; # Please read the comment before changing.
+  home.stateVersion = "24.05"; # NO TOUCHING
 
   programs.kitty.enable = true;
   wayland.windowManager.hyprland.enable = true;
   wayland.windowManager.hyprland.settings = {
+    general = {
+      "sensitivity" = "1.0";
+      "layout" = "dwindle";
+      "col.active_border" = "rgba(000000ff) rgba(ffffffff) 60deg";
+      "col.inactive_border" = "rgba(000000ff)";
+    };
+
+    monitor = ",preferred,auto,1";
+
+    input = {
+      "kb_layout" = "us";
+      "kb_model" = "pc105";
+    };
+
     "$mod" = "SUPER";
+
+    "$term" = "kitty";
+    "$editor" = "nvim";
+    "$file" = "dolphin";
+    "$browser" = "firefox";
+
+    # NOTE: bind[flags]:
+    # l -> locked, will also work when an input inhibitor (e.g. a lockscreen) is active.
+    # r -> release, will trigger on release of a key.
+    # e -> repeat, will repeat when held.
+    # n -> non-consuming, key/mouse events will be passed to the active window in addition to triggering the dispatcher.
+    # m -> mouse, see below.
+    # t -> transparent, cannot be shadowed by other binds.
+    # i -> ignore mods, will ignore modifiers.
+    # s -> separate, will arbitrarily combine keys between each mod/key, see [Keysym combos](#keysym-combos) above.
+    # d -> has description, will allow you to write a description for your bind.
+    # p -> bypasses the app's requests to inhibit keybinds.
 
     bind =
       [
-        "$mod, F, exec, firefox"
-        "$mod, T, exec, kitty"
+        "$mod, Q, killactive"
+        "$mod, T, exec, $term"
+
+        "$mod, D, exec, pkill -x rofi || rofi -show drun"
+        "$mod, S, togglefloating,"
+        "Alt, Return, fullscreen,"
+        "Ctrl+Alt, E, exec, killall waybar || waybar"
+
+        "$mod, E, exec, $file"
+        "$mod, C, exec, $editor"
+        "$mod, F, exec, $browser"
+        "Ctrl+Shift, Escape, exec, btm"
+
+        "$mod, Tab, exec, pkill -x rofi || $scrPath/rofilaunch.sh w # launch window switcher"
+        "$mod+Shift, E, exec, pkill -x rofi || $scrPath/rofilaunch.sh f # launch file explorer"
+
+        "$mod, P, exec, $scrPath/screenshot.sh s # partial screenshot capture"
+        "$mod+Ctrl, P, exec, $scrPath/screenshot.sh sf # partial screenshot capture (frozen screen)"
+        "$mod+Alt, P, exec, $scrPath/screenshot.sh m # monitor screenshot capture"
+        ", Print, exec, $scrPath/screenshot.sh p # all monitors screenshot capture"
+
+        # Custom scripts
+        "$mod+Alt, Right, exec, $scrPath/swwwallpaper.sh -n # next wallpaper"
+        "$mod+Alt, Left, exec, $scrPath/swwwallpaper.sh -p # previous wallpaper"
+        "$mod, V, exec, pkill -x rofi || $scrPath/cliphist.sh c # launch clipboard"
+        "Alt, K, exec, $scrPath/keyboardswitch.sh # switch keyboard layout"
+
+        "Alt, J, togglesplit"
+
+        "$mod, h, movefocus, l"
+        "$mod, j, movefocus, d"
+        "$mod, k, movefocus, u"
+        "$mod, l, movefocus, r"
+
+        "$mod+Shift, h, movewindow, l"
+        "$mod+Shift, j, movewindow, d"
+        "$mod+Shift, k, movewindow, u"
+        "$mod+Shift, l, movewindow, r"
       ]
       ++ (
         builtins.concatLists (builtins.genList (
             i: let
               ws = i + 1;
             in [
-              "$mod, code:1${toString i}, workspace, ${toString ws}"
-              "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+              "$mod, ${toString i}, workspace, ${toString ws}"
+              "$mod SHIFT, ${toString i}, movetoworkspace, ${toString ws}"
+              "$mod ALT, ${toString i}, movetoworkspacesilent, ${toString ws}"
             ]
           )
           9)
       );
+
+    bindm = [
+      "$mod, mouse:272, movewindow"
+      "$mod, mouse:273, resizewindow"
+      "$mod, Z, movewindow"
+      "$mod, X, resizewindow"
+    ];
+
+    bindl = [
+      ", XF86AudioMute, exec, $scrPath/volumecontrol.sh -o m # toggle audio mute"
+      ", XF86AudioMicMute, exec, $scrPath/volumecontrol.sh -i m # toggle microphone mute"
+      ", XF86AudioPlay, exec, playerctl play-pause # toggle between media play and pause"
+      ", XF86AudioPause, exec, playerctl play-pause # toggle between media play and pause"
+      ", XF86AudioNext, exec, playerctl next # media next"
+      ", XF86AudioPrev, exec, playerctl previous # media previous"
+    ];
+
+    bindel = [
+      ", XF86AudioLowerVolume, exec, $scrPath/volumecontrol.sh -o d # decrease volume"
+      ", XF86AudioRaiseVolume, exec, $scrPath/volumecontrol.sh -o i # increase volume"
+      ", XF86MonBrightnessUp, exec, $scrPath/brightnesscontrol.sh i # increase brightness"
+      ", XF86MonBrightnessDown, exec, $scrPath/brightnesscontrol.sh d # decrease brightness"
+    ];
+
+    binde = [
+      "$mod+Alt, h, resizeactive, -30 0"
+      "$mod+Alt, j, resizeactive, 0 30"
+      "$mod+Alt, k, resizeactive, 0 -30"
+      "$mod+Alt, l, resizeactive, 30 0"
+    ];
   };
 
   # The home.packages option allows you to install Nix packages into your
