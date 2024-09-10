4a9638610efafe811447f4b1042668d3452ecd6e
teodord25 <djuric.teodor25@gmail.com>
2024-09-10 09:35:38 +0200

    move nushell config to separate file

--- hosts/main/home.nix
+++ hosts/main/home.nix
@@ -8,68 +8,18 @@
 
   wayland.windowManager.hyprland = import ./hyprland.nix;
 
-  programs = {
-    nushell = {
-      enable = true;
-      # The config.nu can be anywhere you want if you like to edit your Nushell with Nu
-      # configFile.source = ./.../config.nu;
-      # for editing directly to config.nu
-      extraConfig = ''
-        let carapace_completer = {|spans|
-        carapace $spans.0 nushell $spans | from json
-        }
-        $env.config = {
-         show_banner: false,
-         completions: {
-         case_sensitive: false # case-sensitive completions
-         quick: true    # set to false to prevent auto-selecting completions
-         partial: true    # set to false to prevent partial filling of the prompt
-         algorithm: "fuzzy"    # prefix or fuzzy
-         external: {
-         # set to false to prevent nushell looking into $env.PATH to find more suggestions
-             enable: true
-         # set to lower can improve completion performance at the cost of omitting some options
-             max_results: 100
-             completer: $carapace_completer # check 'carapace_completer'
-           }
-         }
-        }
-        $env.PATH = ($env.PATH |
-        split row (char esep) |
-        prepend /home/myuser/.apps |
-        append /usr/bin/env
-        )
+  programs.nushell = import ./nushell.nix;
 
-        def hyprtest [] {
-          mv /home/bane/.config/hypr/hyprland.conf ~/.config/hypr/tmpHypr
-          cp ~/.config/hypr/tmpHypr ~/.config/hypr/hyprland.conf
-          chmod 777 ~/.config/hypr/hyprland.conf
-          nvim ~/.config/hypr/hyprland.conf
-          rm ~/.config/hypr/hyprland.conf
-          mv ~/.config/hypr/tmpHypr ~/.config/hypr/hyprland.conf
-        }
-        alias hyprtest = hyprtest
+  programs.carapace.enable = true;
+  programs.carapace.enableNushellIntegration = true;
 
-      '';
-      shellAliases = {
-        rebuild = "~/nixos/rebuild.nu";
-        ga = "git add";
-        gc = "git commit";
-        gs = "git status";
-        gp = "git push";
-      };
-    };
-    carapace.enable = true;
-    carapace.enableNushellIntegration = true;
-
-    starship = {
-      enable = true;
-      settings = {
-        add_newline = true;
-        character = {
-          success_symbol = "[➜](bold green)";
-          error_symbol = "[➜](bold red)";
-        };
+  programs.starship = {
+    enable = true;
+    settings = {
+      add_newline = true;
+      character = {
+        success_symbol = "[➜](bold green)";
+        error_symbol = "[➜](bold red)";
       };
     };
   };
---
+++ hosts/main/nushell.nix
@@ -0,0 +1,47 @@
+{
+  enable = true;
+  # configFile.source = ./.../config.nu;
+  extraConfig = ''
+	let carapace_completer = {|spans|
+	carapace $spans.0 nushell $spans | from json
+	}
+	$env.config = {
+	 show_banner: false,
+	 completions: {
+	 case_sensitive: false # case-sensitive completions
+	 quick: true    # set to false to prevent auto-selecting completions
+	 partial: true    # set to false to prevent partial filling of the prompt
+	 algorithm: "fuzzy"    # prefix or fuzzy
+	 external: {
+	 # set to false to prevent nushell looking into $env.PATH to find more suggestions
+		 enable: true
+	 # set to lower can improve completion performance at the cost of omitting some options
+		 max_results: 100
+		 completer: $carapace_completer # check 'carapace_completer'
+	   }
+	 }
+	}
+	$env.PATH = ($env.PATH |
+	split row (char esep) |
+	prepend /home/myuser/.apps |
+	append /usr/bin/env
+	)
+
+	def hyprtest [] {
+	  mv /home/bane/.config/hypr/hyprland.conf ~/.config/hypr/tmpHypr
+	  cp ~/.config/hypr/tmpHypr ~/.config/hypr/hyprland.conf
+	  chmod 777 ~/.config/hypr/hyprland.conf
+	  nvim ~/.config/hypr/hyprland.conf
+	  rm ~/.config/hypr/hyprland.conf
+	  mv ~/.config/hypr/tmpHypr ~/.config/hypr/hyprland.conf
+	}
+	alias hyprtest = hyprtest
+
+  '';
+  shellAliases = {
+	rebuild = "~/nixos/rebuild.nu";
+	ga = "git add";
+	gc = "git commit";
+	gs = "git status";
+	gp = "git push";
+}
