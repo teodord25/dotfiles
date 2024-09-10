fba7304fdb2313fe61cec1ef734c164c6ad0f5cd
teodord25 <djuric.teodor25@gmail.com>
2024-09-10 11:41:09 +0200

    add plugin dir

---
+++ hosts/main/nvim/plugin/lsp.lua
@@ -0,0 +1,51 @@
+-- on_attach is called after the lsp server starts and connects to neovim
+local on_attach = function(_, buffer)
+    local bufmap = function(keys, func)
+        vim.keymap.set('n', keys, func, { buffer = buffer })
+    end
+
+    bufmap('<leader>r', vim.lsp.buf.rename)
+    bufmap('<leader>a', vim.lsp.buf.code_action)
+
+    bufmap('gd', vim.lsp.buf.definition)
+    bufmap('gD', vim.lsp.buf.declaration)
+    bufmap('gI', vim.lsp.buf.implementation)
+    bufmap('<leader>D', vim.lsp.buf.type_definition)
+
+    bufmap('K', vim.lsp.buf.hover)
+
+    bufmap('gr', require('telescope.builtin').lsp_references)
+    bufmap('<leader>s', require('telescope.builtin').lsp_document_symbols)
+    bufmap('<leader>S', require('telescope.builtin').lsp_dynamic_workspace_symbols)
+
+    vim.api.nvim_buf_create_user_command(buffer, 'Format', function(_)
+        vim.lsp.buf.format()
+    end, {})
+end
+
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
+require("mason").setup()
+require("mason-lspconfig").setup_handlers({
+    -- general handler for undefined stuff idk
+    function(server_name)
+        require("lspconfig")[server_name].setup {
+            on_attach = on_attach,
+            capabilities = capabilities,
+        }
+    end,
+
+    ["lua_ls"] = function()
+        require("neodev").setup()
+        require("lspconfig").lua_ls.setup {
+            on_attach = on_attach,
+            capabilities = capabilities,
+            Lua = {
+                workspace = { checkThirdParty = false },
+                telemetry = { enable = false },
+            },
+        }
+    end
+})
