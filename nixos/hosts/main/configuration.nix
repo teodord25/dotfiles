ded838e764db80f00cca503beac7bb50773bc69d
teodord25 <djuric.teodor25@gmail.com>
2024-09-22 16:23:58 +0200

    NixOS Rebuild - Generation 166

--- nixos/hosts/main/configuration.nix
+++ nixos/hosts/main/configuration.nix
@@ -88,6 +88,28 @@
     yazi
     mpv
 
+    bat
+    difftastic
+    zoxide
+    xcp
+    dysk
+    dust
+    erdtree
+    fd
+    procs
+    rm-improved
+    sd
+    tailspin
+    spacer
+    csvlens
+    dog
+    fselect
+    htmlq
+    inlyne
+    html-tidy
+    zet
+    xh
+
     file
 
     cargo
