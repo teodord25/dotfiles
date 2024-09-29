2853c5344c6325c8c1155e696cfecda2b8ee04eb
teodord25 <djuric.teodor25@gmail.com>
2024-09-29 14:26:06 +0200

    NixOS Rebuild - Generation 189

--- nixos/hosts/main/home.nix
+++ nixos/hosts/main/home.nix
@@ -106,7 +106,7 @@
   #  /etc/profiles/per-user/bane/etc/profile.d/hm-session-vars.sh
   #
   home.sessionVariables = {
-    EDITOR = "nvim";
+    # EDITOR = "nvim";
   };
 
   # Let Home Manager install and manage itself.
