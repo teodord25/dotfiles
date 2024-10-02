673d73df4a9619b63c98f66fdbbda082047e20ec
teodord25 <djuric.teodor25@gmail.com>
2024-10-02 13:20:10 +0200

    fix vimtex import

--- other/nvim/lua/pluginlist.lua
+++ other/nvim/lua/pluginlist.lua
@@ -174,8 +174,9 @@ return {
 	{
 		"lervag/vimtex",
 		lazy = false, -- we don't want to lazy load VimTeX
-		init = function()
+		config = function()
 			vim.g.vimtex_view_method = "zathura"
+			vim.g.vimtex_compiler_method = "latexmk"
 		end
 	}
 }
