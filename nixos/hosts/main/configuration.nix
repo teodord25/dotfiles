e50c9251fffd3e2ad508e48f3bfce88f26272b4d
teodord25 <djuric.teodor25@gmail.com>
2024-11-19 11:06:08 +0100

    NixOS Rebuild - Generation 4

--- nixos/hosts/main/configuration.nix
+++ nixos/hosts/main/configuration.nix
@@ -33,6 +33,13 @@
 
   programs.gnupg.agent.enable = true;
 
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
+
   main-user.enable = true;
   main-user.userName = "bane";
 
