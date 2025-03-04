6993fbaa3fb136350e1dab29cca6f770cdb97ca6
teodord25 <djuric.teodor25@gmail.com>
2025-03-04 16:04:16 +0100

    flake??

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -13,45 +13,34 @@
     alejandra.inputs.nixpkgs.follows = "nixpkgs";
 
     templ.url = "github:a-h/templ";
-
     hyprland.url = "github:hyprwm/Hyprland";
-
     ags.url = "github:aylur/ags/v1";
-
     ghostty.url = "github:ghostty-org/ghostty";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    alejandra,
-    templ,
-    ghostty,
-    ...
-  } @ inputs: {
-    nixosConfigurations = {
-      main = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs;};
+  outputs = { self, nixpkgs, ... } @ inputs: {
+    nixosConfigurations.main = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";  # Explicit system architecture
+      specialArgs = { inherit inputs; };
 
-        modules = [
-          ./hosts/main/configuration.nix
-          inputs.home-manager.nixosModules.default
+      modules = [
+        ./hosts/main/configuration.nix
+        inputs.home-manager.nixosModules.default
 
-          ({pkgs, ...}: {
-            nixpkgs.overlays = [
-              inputs.templ.overlays.default
-              inputs.ghostty.overlays.default  # Ghostty overlay
-            ];
+        ({ pkgs, ... }: {
+          nixpkgs.overlays = [
+            inputs.templ.overlays.default
+            inputs.ghostty.overlays.default
+          ];
 
-            environment.systemPackages = with pkgs; [
-              templ
-              ghostty  # From overlay
-            ] ++ [
-              inputs.alejandra.defaultPackage.${pkgs.system}  # From input
-            ];
-          })
-        ];
-      };
+          environment.systemPackages = with pkgs; [
+            templ
+            ghostty
+          ] ++ [
+            inputs.alejandra.defaultPackage.${pkgs.system}
+          ];
+        })
+      ];
     };
   };
 }
