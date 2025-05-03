f809bfe64f516c6dd0a88e1819fb2284347d01a6
teodord25 <djuric.teodor25@gmail.com>
2025-05-03 03:08:43 +0200

    NixOS Rebuild - Generation 53

--- nixos/hosts/main/configuration.nix
+++ nixos/hosts/main/configuration.nix
@@ -47,30 +47,30 @@
 
   services.blueman.enable = true;
 
-  hardware.bluetooth.settings = {
-    General = {
-      Enable = "Source,Sink,Media,Socket";
-    };
-  };
-
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    wireplumber.configPackages = [
-      (pkgs.writeTextDir "share/wireplumber/wireplumber.conf.d/10-bluez.conf" ''
-        monitor.bluez.properties = {
-          bluez5.roles = [ a2dp_sink a2dp_source bap_sink bap_source hsp_hs hsp_ag hfp_hf hfp_ag ]
-          bluez5.codecs = [ sbc sbc_xq aac ldac ]
-          bluez5.enable-sbc-xq = true
-          bluez5.hfphsp-backend = "native"
-        }
-      '')
-    ];
-  };
+  # hardware.bluetooth.settings = {
+  #   General = {
+  #     Enable = "Source,Sink,Media,Socket";
+  #   };
+  # };
+  #
+  # hardware.pulseaudio.enable = false;
+  # security.rtkit.enable = true;
+  # services.pipewire = {
+  #   enable = true;
+  #   alsa.enable = true;
+  #   alsa.support32Bit = true;
+  #   pulse.enable = true;
+  #   wireplumber.configPackages = [
+  #     (pkgs.writeTextDir "share/wireplumber/wireplumber.conf.d/10-bluez.conf" ''
+  #       monitor.bluez.properties = {
+  #         bluez5.roles = [ a2dp_sink a2dp_source bap_sink bap_source hsp_hs hsp_ag hfp_hf hfp_ag ]
+  #         bluez5.codecs = [ sbc sbc_xq aac ldac ]
+  #         bluez5.enable-sbc-xq = true
+  #         bluez5.hfphsp-backend = "native"
+  #       }
+  #     '')
+  #   ];
+  # };
 
   time.timeZone = "Europe/Belgrade";
 
