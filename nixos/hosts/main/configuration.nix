601675e4a12feb81a56016d6192d33624fa07e13
teodord25 <djuric.teodor25@gmail.com>
2025-05-03 02:44:10 +0200

    NixOS Rebuild - Generation 50

--- nixos/hosts/main/configuration.nix
+++ nixos/hosts/main/configuration.nix
@@ -40,12 +40,28 @@
   # Enable networking
   networking.networkmanager.enable = true;
 
-  services.blueman.enable = true;
-
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = true;
   };
+
+  services.blueman.enable = true;
+
+  hardware.bluetooth.settings = {
+    General = {
+      Enable = "Source,Sink,Media,Socket";
+    };
+  };
+
+  hardware.pulseaudio.configFile = pkgs.writeText "default.pa" ''
+    load-module module-bluetooth-policy
+    load-module module-bluetooth-discover
+    ## module fails to load with
+    ##   module-bluez5-device.c: Failed to get device path from module arguments
+    ##   module.c: Failed to load module "module-bluez5-device" (argument: ""): initialization failed.
+    # load-module module-bluez5-device
+    # load-module module-bluez5-discover
+  '';
 
   services.pipewire = {
     enable = true;
