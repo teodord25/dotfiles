7e38d749b143f4e597e046f426ed8076f11ae66e
teodord25 <djuric.teodor25@gmail.com>
2025-06-28 22:16:30 +0200

    NixOS Rebuild - Generation 83

--- other/nvim/lua/pluginlist.lua
+++ other/nvim/lua/pluginlist.lua
@@ -8,6 +8,10 @@ return {
 			{ 'hat0uma/csvview.nvim',         lazy = true },
 		},
 		opts = {},
+		keys = {
+			{ "<leader>po", "<cmd>OmniPreview start<CR>", desc = "OmniPreview Start" },
+			{ "<leader>pc", "<cmd>OmniPreview stop<CR>",  desc = "OmniPreview Stop" },
+		},
 	},
 	{
 		"mfussenegger/nvim-jdtls",
