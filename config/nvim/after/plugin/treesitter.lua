6d32025046cf798d91778fae2bec8955f8fe2ecc
teodord25 <djuric.teodor25@gmail.com>
2025-07-14 19:02:43 +0200

    rm setup.sh; add TODO counter to starship

--- config/nvim/after/plugin/treesitter.lua
+++ config/nvim/after/plugin/treesitter.lua
@@ -1,7 +1,7 @@
 require('nvim-treesitter.configs').setup {
-    ensure_installed = { 'vim', 'vimdoc', 'lua', 'nix' },
+	ensure_installed = { 'vim', 'vimdoc', 'lua', 'nix', 'nu' },
 
-    auto_install = false,
-    highlight = { enable = true },
-    indent = { enable = true },
+	auto_install = false,
+	highlight = { enable = true },
+	indent = { enable = true },
 }
