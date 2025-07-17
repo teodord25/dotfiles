cab47854d0673a40493cb1e257f986fa98925586
teodord25 <djuric.teodor25@gmail.com>
2025-07-17 19:03:38 +0200

    add git switch alias; flake update

--- flakes/git-aliases/flake.nix
+++ flakes/git-aliases/flake.nix
@@ -1,15 +1,18 @@
 {
   description = "My shared shell aliases as an overlay";
 
-  outputs = { self }: {
-    overlays.default = final: prev: {
-      sharedAliases = ''
-        alias gs='git status'
-        alias gc='git commit'
-        alias gp='git push'
-        alias gl='git log'
-        alias ga='git add'
-      '';
+  outputs =
+    { self }:
+    {
+      overlays.default = final: prev: {
+        sharedAliases = ''
+                  alias gs='git status'
+                  alias gc='git commit'
+                  alias gp='git push'
+                  alias gl='git log'
+                  alias ga='git add'
+                  alias gw='git switch'
+        '';
+      };
     };
-  };
 }
