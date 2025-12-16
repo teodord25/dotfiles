b89b903783ddc1606e66578d59b085d66b51adce
teodord25 <djuric.teodor25@gmail.com>
2025-12-16 22:23:19 +0100

    chore(todo): add gleam task

--- config/nvim/lua/config/lsp/defaults.lua
+++ config/nvim/lua/config/lsp/defaults.lua
@@ -2,7 +2,10 @@ vim.lsp.enable("rust_analyzer")
 vim.lsp.enable("lua_ls")
 vim.lsp.enable("clangd")
 vim.lsp.enable("intelephense")
+
+-- TODO: fix gleam ls
 vim.lsp.enable("gleam")
+
 vim.lsp.enable("gopls")
 vim.lsp.enable("basedpyright")
 vim.lsp.enable("nil_ls")
