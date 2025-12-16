7d04cf96a8abf6f75a9e69c5268283edceeda962
teodord25 <djuric.teodor25@gmail.com>
2025-12-16 22:56:08 +0100

    replace roslyn

--- config/nvim/lua/config/lsp/custom/cs_ls.lua
+++ config/nvim/lua/config/lsp/custom/cs_ls.lua
@@ -1,7 +1,11 @@
 vim.lsp.config.cs_ls = {
-	cmd = { "Microsoft.CodeAnalysis.LanguageServer" },
+	cmd = { 
+		"Microsoft.CodeAnalysis.LanguageServer",
+		"--logLevel", "Information",
+		"--extensionLogDirectory", vim.fn.stdpath("cache") .. "/roslyn_logs"
+	},
 	filetypes = { "cs" },
-	root_markers = { ".git" },
+	root_markers = { ".git", "*.sln", "*.csproj" },
 }
 
 vim.lsp.enable("cs_ls")
