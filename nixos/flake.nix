a1757f40a32f6a8c1f7638613b72be5bc4540c78
teodord25 <djuric.teodor25@gmail.com>
2026-01-26 23:22:50 +0100

    fix description and unnecessary nesting

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,14 +1,12 @@
 {
-  description = "NixOS config flake";
+  description = "NixOS system flake";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-      };
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     alejandra = {
