5f115feca42f237a768c9ed4bdeb796e9dbeb520
teodord25 <djuric.teodor25@gmail.com>
2026-01-27 18:50:00 +0100

    add comment

--- setup.sh
+++ setup.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
+
+# this is broken idk
 set -euo pipefail
 
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 SYSTEMD_USER_DIR="$HOME/.config/systemd/user"
 
-bash "$ROOT_DIR/other/setup.sh"
+bash "$ROOT_DIR/other/setup.sh" # what was this
 
 mkdir -p "$SYSTEMD_USER_DIR"
 
