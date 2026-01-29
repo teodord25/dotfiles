255ef41523ee65fc4d2e3bcd6c3c5c160744fed9
teodord25 <djuric.teodor25@gmail.com>
2026-01-29 11:07:01 +0100

    rm ft java thing; refactor lsp.lua

--- config/nvim/after/plugin/lsp.lua
+++ config/nvim/after/plugin/lsp.lua
@@ -1,6 +1,3 @@
--- load custom LSP configs
-local custom_configs_path = vim.fn.stdpath("config") .. "/lua/config/lsp/custom"
-
 -- load global configuration
 require("config.lsp.global")
 
@@ -8,7 +5,11 @@ require("config.lsp.global")
 require("config.lsp.defaults")
 
 -- then load custom configs
-for _, file in ipairs(vim.fn.readdir(custom_configs_path)) do
+for _, file in ipairs(
+  vim.fn.readdir(
+    vim.fn.stdpath("config") .. "/lua/config/lsp/custom"
+  )
+) do
   if file:match("%.lua$") then
     local module_name = "config.lsp.custom." .. file:gsub("%.lua$", "")
     require(module_name)
