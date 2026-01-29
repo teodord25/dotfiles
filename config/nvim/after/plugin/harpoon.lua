208cc8e25055e81e6f70092af4404e1f5c19148b
teodord25 <djuric.teodor25@gmail.com>
2026-01-29 11:13:37 +0100

    change harpoon binds

--- config/nvim/after/plugin/harpoon.lua
+++ config/nvim/after/plugin/harpoon.lua
@@ -4,8 +4,8 @@ local harpoon_ui = require('harpoon.ui')
 local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
-map('n', '<leader>a', function() harpoon_mark.add_file() end, opts)
-map('n', '<C-e>', function() harpoon_ui.toggle_quick_menu() end, opts)
+map('n', '<M-a>', function() harpoon_mark.add_file() end, opts)
+map('n', '<M-e>', function() harpoon_ui.toggle_quick_menu() end, opts)
 
 map('n', '<M-h>', function() harpoon_ui.nav_file(1) end, opts)
 map('n', '<M-j>', function() harpoon_ui.nav_file(2) end, opts)
