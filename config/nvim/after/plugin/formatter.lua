11523a1f03a4408975b35809cc779e68f71ef6b0
Teodor <djuric.teodor25@gmail.com>
2026-02-05 12:15:08 +0100

    set up ruff linting and 80ch limit

--- config/nvim/after/plugin/formatter.lua
+++ config/nvim/after/plugin/formatter.lua
@@ -1,9 +1,16 @@
 require("conform").setup({
   formatters_by_ft = {
     json = { "prettier" },
+    python = { "ruff_format" },
   },
 
   default_format_opts = {
     lsp_format = "fallback", -- use LSP when formatter isnt set
+  },
+
+  formatters = {
+    ruff_format = {
+      prepend_args = { "--line-length", "80" },
+    },
   },
 })
