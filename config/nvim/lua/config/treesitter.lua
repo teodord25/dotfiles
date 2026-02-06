dc6dbd636f92546bdf2256db634aa2305ed125e5
Teodor <djuric.teodor25@gmail.com>
2026-02-06 22:03:03 +0100

    set up vim grammar for tree sitter for tridactylrc

--- config/nvim/lua/config/treesitter.lua
+++ config/nvim/lua/config/treesitter.lua
@@ -1,5 +1,5 @@
 -- TODO: move everything into /lua/config
--- and then require() each setup file in 
+-- and then require() each setup file in
 -- each appropriate plugin declaration
 
 require('nvim-treesitter').setup({
@@ -7,4 +7,13 @@ require('nvim-treesitter').setup({
 	auto_install = false,
 	highlight = { enable = true },
 	indent = { enable = true },
+})
+
+vim.filetype.add({
+	filename = {
+		['tridactylrc'] = 'vim',
+	},
+	pattern = {
+		['.*%.tridactylrc'] = 'vim',
+	},
 })
