6e2b36f2b86e9320253327785979213266a5e697
Teodor <djuric.teodor25@gmail.com>
2026-02-07 15:07:16 +0100

    add "WHAT" comment type

--- config/nvim/lua/pluginlist.lua
+++ config/nvim/lua/pluginlist.lua
@@ -152,6 +152,7 @@ return {
 				PERF = { icon = " ", color = "default", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
 				NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
 				TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+				WHAT = { icon = "‽", color = "error" },
 			},
 			gui_style = {
 				fg = "NONE",         -- The gui style to use for the fg highlight group.
