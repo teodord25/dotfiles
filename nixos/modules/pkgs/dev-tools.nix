bd6553ed1e27275867eaba10a2ad137958435868
Teodor <djuric.teodor25@gmail.com>
2026-05-25 16:54:39 +0200

    move dev tools to dev-tools.nix

---
+++ nixos/modules/pkgs/dev-tools.nix
@@ -0,0 +1,38 @@
+{pkgs, ...}: {
+  environment.systemPackages = with pkgs; [
+    taplo
+    tig
+    python3Packages.pip
+    cacert
+    vscode-langservers-extracted
+    typst
+    typstPackages."modern-cv"
+
+    sqlitebrowser
+
+    dnsutils
+    flyctl
+    qemu
+    virt-manager # qemu wrapper
+    zathura
+
+    inlyne
+
+    dbeaver-bin
+
+    prettier
+    ruff
+
+    entr
+    delta
+    tmuxinator
+
+
+    # opencode
+
+    luajit
+    luajitPackages.lgi
+
+    nushell
+  ];
+}
--- nixos/modules/pkgs/tools.nix
+++ nixos/modules/pkgs/tools.nix
@@ -20,40 +20,5 @@
     ripgrep
     tmux
     p7zip
-
-    taplo
-    tig
-    python3Packages.pip
-    cacert
-    vscode-langservers-extracted
-    typst
-    typstPackages."modern-cv"
-
-    sqlitebrowser
-
-    dnsutils
-    flyctl
-    qemu
-    virt-manager # qemu wrapper
-    zathura
-
-    inlyne
-
-    dbeaver-bin
-
-    prettier
-    ruff
-
-    entr
-    delta
-    tmuxinator
-
-
-    # opencode
-
-    luajit
-    luajitPackages.lgi
-
-    nushell
   ];
 }
