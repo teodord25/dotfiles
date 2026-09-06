a6642221f12faffbc6f6f2b4c1c531e1075461fe
Teodor <djuric.teodor25@gmail.com>
2026-09-06 13:26:01 +0200

    actually fix

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -32,7 +32,7 @@
     claude-code-nix.url = "github:sadjow/claude-code-nix";
     # follows excluded on purpose
 
-    inputs.diane = {
+    diane = {
       url = "git+ssh://git@github.com/teodord25/diane.git";
       inputs.nixpkgs.follows = "nixpkgs";
     };
--- nixos/modules/packages/diane.nix
+++ nixos/modules/packages/diane.nix
@@ -1,15 +1,6 @@
-{
-  pkgs,
-  inputs,
-  ...
-}: {
-  # Packages exposed directly by flake inputs (not via an overlay). Previously
-  # these were listed inline in each nixosConfiguration in flake.nix.
+{ pkgs, inputs, ... }: {
   environment.systemPackages = [
-    inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
-    inputs.alejandra.packages.${pkgs.stdenv.hostPlatform.system}.default
+    inputs.diane.packages.${pkgs.stdenv.hostPlatform.system}.default
   ];
-
-  environment.systemPackages = [ inputs.diane.packages.${pkgs.system}.default ];
-  environment.variables.DIANE_VAULT = "/home/${config.main-user}/vault";
+  environment.sessionVariables.DIANE_VAULT = "$HOME/vault";
 }
