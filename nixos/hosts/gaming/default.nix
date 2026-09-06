4e8b0856c452f4ba9c0cc925c7f651b20c67490c
Teodor <djuric.teodor25@gmail.com>
2026-09-06 15:05:01 +0200

    anton.nix

--- nixos/hosts/gaming/default.nix
+++ nixos/hosts/gaming/default.nix
@@ -10,6 +10,7 @@
     ../../modules/profiles/vpn.nix
     ../../modules/profiles/rust.nix
     ../../modules/profiles/tailnet-host.nix
+    ../../modules/profiles/anton.nix
   ];
 
   networking.hostName = "teodor-gaming-nixos";
@@ -21,28 +22,4 @@
   environment.systemPackages = [pkgs.templ]; # from inputs.templ overlay
 
   system.stateVersion = "24.05";
-
-
-
-# { pkgs, inputs, ... }: {
-  systemd.services.llama = {
-    description = "llama-server for Anton";
-    wantedBy = [ "multi-user.target" ];
-    after = [ "network-online.target" ];
-    wants = [ "network-online.target" ];
-    environment = {
-      DIANE_LLM_HOST = "0.0.0.0";
-      DIANE_GGUF = "/home/bane/.local/share/diane/Qwen2.5-14B-Instruct-Q4_K_M.gguf";
-      DIANE_CTX = "8192";
-    };
-    serviceConfig = {
-      ExecStart = "${inputs.diane.packages.${pkgs.stdenv.hostPlatform.system}.llama-server}/bin/diane-llama-server";
-      User = "bane";
-      SupplementaryGroups = [ "video" "render" ];
-      Restart = "on-failure";
-      RestartSec = 5;
-    };
-  };
-# }
-
 }
---
+++ nixos/modules/profiles/anton.nix
@@ -0,0 +1,20 @@
+{ pkgs, inputs, ... }: {
+  systemd.services.llama = {
+    description = "llama-server for Anton";
+    wantedBy = [ "multi-user.target" ];
+    after = [ "network-online.target" ];
+    wants = [ "network-online.target" ];
+    environment = {
+      DIANE_LLM_HOST = "0.0.0.0";
+      DIANE_GGUF = "/home/bane/.local/share/diane/Qwen2.5-14B-Instruct-Q4_K_M.gguf";
+      DIANE_CTX = "8192";
+    };
+    serviceConfig = {
+      ExecStart = "${inputs.diane.packages.${pkgs.stdenv.hostPlatform.system}.llama-server}/bin/diane-llama-server";
+      User = "bane";
+      SupplementaryGroups = [ "video" "render" ];
+      Restart = "on-failure";
+      RestartSec = 5;
+    };
+  };
+}
