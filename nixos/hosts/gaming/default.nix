a935b787d8a0efc2ad2aae04dc67455e21e5b0c9
Teodor <djuric.teodor25@gmail.com>
2026-09-06 15:03:36 +0200

    add llama service

--- nixos/hosts/gaming/default.nix
+++ nixos/hosts/gaming/default.nix
@@ -21,4 +21,28 @@
   environment.systemPackages = [pkgs.templ]; # from inputs.templ overlay
 
   system.stateVersion = "24.05";
+
+
+
+# { pkgs, inputs, ... }: {
+  systemd.services.llama = {
+    description = "llama-server for Anton";
+    wantedBy = [ "multi-user.target" ];
+    after = [ "network-online.target" ];
+    wants = [ "network-online.target" ];
+    environment = {
+      DIANE_LLM_HOST = "0.0.0.0";
+      DIANE_GGUF = "/home/bane/.local/share/diane/Qwen2.5-14B-Instruct-Q4_K_M.gguf";
+      DIANE_CTX = "8192";
+    };
+    serviceConfig = {
+      ExecStart = "${inputs.diane.packages.${pkgs.stdenv.hostPlatform.system}.llama-server}/bin/diane-llama-server";
+      User = "bane";
+      SupplementaryGroups = [ "video" "render" ];
+      Restart = "on-failure";
+      RestartSec = 5;
+    };
+  };
+# }
+
 }
